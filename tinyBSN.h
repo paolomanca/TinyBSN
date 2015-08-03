c0e0a0b85ea377ad38db95633484edce4413441b
Paolo Manca <paolo@manca.eu>
2015-08-03 12:59:22 +0200

    Added costant for number of PNs

--- tinyBSN.h
+++ tinyBSN.h
@@ -1,4 +1,4 @@
-/**
+﻿﻿/**
  *  @author Paolo Manca
  */
 
@@ -7,13 +7,15 @@
 
 #define BUF_SIZE 200
 
+#define N_PNS 4
+
 /** Classification thresholds **/
 #define M_THR 0.5
 #define C_THR 2
 
 /** Message types **/
 #define REQ 1
-#define RESP 2
+#define RES 2
 
 /** Message values **/
 #define START 0
@@ -22,15 +24,15 @@
 #define CRISIS 3
 
 typedef nx_struct my_msg {
-	nx_uint8_t msg_type;
-	nx_uint16_t msg_id;
-	nx_uint8_t value;
+		nx_uint8_t msg_type;
+		nx_uint16_t msg_id;
+		nx_uint8_t value;
 } my_msg_t;
 
 
 
 enum{
-AM_MY_MSG = 6,
+	AM_MY_MSG = 6,
 };
 
 #endif
